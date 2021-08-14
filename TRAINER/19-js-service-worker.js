0ccf9a010f47ad631b90617d1e04794ab9b424cb
Vadzim Naumchyk <vadzimn@ppedv.de>
2021-08-14 12:30:16 +0200

    todo: anzeige aus cache der eigenen seite bei offline #2

--- TRAINER/19-js-service-worker.js
+++ TRAINER/19-js-service-worker.js
@@ -1,49 +1,73 @@
-const cacheName = 'v2';
+const cacheName = "v1";
 const cacheAssets = [
-    '19-js-service-worker-main.html',
-    '19-js-service-worker.js',
-    '15-html-audio.html'
-]
+  "19-js-service-worker-main.html",
+  "15-html-audio.html",
+  "19-js-service-worker-offline.html",
+];
 
 // Es gibt kein Event Register, sondern Install
-self.addEventListener('install', (e) => {
-    console.log('Service Worker: Installed');
-    e.waitUntil(
-        caches
-        .open(cacheName)
-        .then(cache => {
-            cache.addAll(cacheAssets);
-            console.log('Service Worker: Caching Files');            
+self.addEventListener("install", (e) => {
+  console.log("Service Worker: Installed");
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("Service Worker: Caching Files");
+      return cache.addAll(cacheAssets);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Lösche alle caches, die dem aktuellen cach-Namen nicht entsprechen, beim activate-event
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker: Activated");
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            // alles, was dem oben in der Zeile 1 beim cacheName nicht entspricht, wird gelöscht
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cache);
+          }
         })
-        .then(() => self.skipWaiting())
-    )
-})
+      );
+    })
+  );
+});
 
-self.addEventListener('activate', (e) => {
-    console.log('Service Worker: Activated');
-    // Remove unwanted caches
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if(cache !== cacheName) { // alles, was dem oben in der Zeile 1 beim cacheName nicht entspricht, wird gelöscht
-                        console.log('Service Worker: Clearing Old Cache');
-                        return caches.delete(cache);
-                    }
-                })
-            )
-        })
-    )
-})
+self.addEventListener("fetch", (e) => {
+  // todo #2
 
-
-self.addEventListener('fetch', e => {
-    // todo #2
-    // in Firefox sieht man diese Meldung. In Edge und Chrome nicht
-    console.log('Service Worker: Fetching');
-    alert('Service Worker: Fetching');
-    e.respondWith(       
-        // bei misserfolg lade assets vom cache
-        fetch(e.request).catch(() => caches.match(e.request))
-    )
-})
+  console.log("Service Worker: Fetching"); // Diese Meldung wird schnell überschrieben, wenn SW bei online neu registriert wird.
+  e.respondWith(
+    // bei misserfolg lade assets vom cache
+    fetch(e.request).
+    catch(() => {
+      return caches.open(cacheName)
+      .then((caches) => {
+        // return      caches.match(e.request)
+        console.log("e.request :>> ", e.request);
+        /*
+        Request {
+            bodyUsed: false
+            cache: "no-cache"
+            credentials: "include"
+            destination: "document"
+            headers: Headers {}
+            integrity: ""
+            isHistoryNavigation: false
+            keepalive: false
+            method: "GET"
+            mode: "navigate"
+            redirect: "manual"
+            referrer: "http://127.0.0.1:8887/"
+            referrerPolicy: "strict-origin-when-cross-origin"
+            signal: AbortSignal {aborted: false, onabort: null}
+            url: "http://127.0.0.1:8887/19-js-service-worker-main.html"
+        }
+        */
+        return caches.match("19-js-service-worker-offline.html");
+      });
+    })
+  );
+});
